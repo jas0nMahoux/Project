3e8c5a46e3eb69826be9bc0f7ab5bc134b0f09c7
jas0nMahoux <56009900+jas0nMahoux@users.noreply.github.com>
2022-10-22 11:47:45 +1100

    dependencies

---
+++ src/main/java/nc/project/start/user/User.java
@@ -0,0 +1,7 @@
+package nc.project.start.user;
+
+import lombok.Data;
+
+@Data
+public class User {
+}
---
+++ src/main/java/nc/project/start/user/UserController.java
@@ -0,0 +1,7 @@
+package nc.project.start.user;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+}
